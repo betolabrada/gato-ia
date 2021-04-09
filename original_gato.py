999a41b76f2aed4ad18810698768acaf15434c2e
Alan Gonzalez <argaalan@hotmail.com>
2021-04-08 22:09:04 -0600

    Moves prepared based on the board played

--- original_gato.py
+++ original_gato.py
@@ -2,6 +2,7 @@
 # Packages to import
 from easyAI import TwoPlayersGame, AI_Player, Negamax
 from easyAI.Player import Human_Player
+import random
 
 
 # Class that contains all methods to play the game
@@ -14,14 +15,40 @@ class GameController(TwoPlayersGame):
         # Define the board
         self.board = [[0 for x in range(9)] for y in range(9)]
 
+        self.actualBoard = -1
+
+        self.closedBoards = []
+
+        self.IAWonBoards = []
+
+        self.PlayerWonBoards = []
+
     # Define possible moves
     def possible_moves(self):
         # Initial [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        return [a + 1 for a, b in enumerate(self.board[0]) if b == 0]
+        # actualBoard is -1 when the player or IA can choose any board, but for this game that would be random
+        if self.actualBoard != -1:
+            moves = [a + 1 for a, b in enumerate(self.board[self.actualBoard]) if b == 0]
+            if not moves:
+                self.closedBoards.append(self.actualBoard)
+                if len(self.closedBoards) < 9:
+                    self.actualBoard = random.randint(0, 8)
+                    while not self.closedBoards.__contains__(self.actualBoard):
+                        self.actualBoard = random.randint(0, 8)
+                    return [a + 1 for a, b in enumerate(self.board[self.actualBoard]) if b == 0]
+                else:
+                    return []
+        else:
+            self.actualBoard = random.randint(0, 8)
+            while not self.closedBoards.__contains__(self.actualBoard):
+                self.actualBoard = random.randint(0, 8)
+            return [a + 1 for a, b in enumerate(self.board[self.actualBoard]) if b == 0]
+        print('Playing on board ' + self.actualBoard)
 
     # Update the board after making a move
     def make_move(self, move):
-        self.board[0][int(move) - 1] = self.nplayer
+        self.board[self.actualBoard][int(move) - 1] = self.nplayer
+        self.actualBoard = int(move) - 1
 
     # Does the opponent have three in line?
     def loss_condition(self):
@@ -30,23 +57,65 @@ class GameController(TwoPlayersGame):
         diagonales = [[1, 5, 9], [3, 5, 7]]
         possible_combinations = horizontales + verticales + diagonales
 
-        return any([all([(self.board[0][i - 1] == self.nopponent) for i in combination]) for combination in
-                    possible_combinations])
+        return any(
+            [all([(self.board[self.actualBoard][i - 1] == self.nopponent) for i in combination]) for combination in
+             possible_combinations])
+
+    def checkWinIA(self):
+        horizontales = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        verticales = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+        diagonales = [[1, 5, 9], [3, 5, 7]]
+        possible_combinations = horizontales + verticales + diagonales
+
+        for i in range(len(possible_combinations)):
+            won = True
+            for j in range(len(possible_combinations[i])):
+                if not self.IAWonBoards.__contains__(possible_combinations[i][j]):
+                    won = False
+                    break
+            if won == 1:
+                return True
+
+        return False
+
+    def checkWinPlayer(self):
+        horizontales = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        verticales = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+        diagonales = [[1, 5, 9], [3, 5, 7]]
+        possible_combinations = horizontales + verticales + diagonales
+
+        for i in range(len(possible_combinations)):
+            won = True
+            for j in range(len(possible_combinations[i])):
+                if not self.PlayerWonBoards.__contains__(possible_combinations[i][j]):
+                    won = False
+                    break
+            if won == 1:
+                return True
+
+        return False
 
     # Check if game is over
     def is_over(self):
-        return (self.possible_moves() == []) or self.loss_condition()
+        return (len(self.closedBoards) == 9) or self.checkWinIA() or self.checkWinPlayer()
 
     # Show current position
     def show(self):
         for h in range(3):
             print('\n' + '\n'.join(
-                [' '.join([['.', 'O', 'X'][self.board[k + h][3 * j + i]] for k in range(3) for i in range(3)]) for j in
+                [' '.join([['.', 'O', 'X'][self.board[k + h * 3][3 * j + i]] for k in range(3) for i in range(3)]) for j
+                 in
                  range(3)]))
 
     # Compute the score
     def scoring(self):
-        return -100 if self.loss_condition() else 0
+
+        if self.checkWinPlayer():
+            return 1
+        elif self.checkWinIA():
+            return -1
+        else:
+            return 0
 
 
 if __name__ == "__main__":
